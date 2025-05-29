a5b9b9cbd684e829f824d9b5e9793822bddb7f27
seyi.idowu <shezi357412@icloud.com>
2025-05-29 21:40:23 +0100

    refactor: clear blocked and allowed categories in updateCard service

--- src/modules/Card/services/updateCard.service.ts
+++ src/modules/Card/services/updateCard.service.ts
@@ -76,8 +76,8 @@ export class UpdateCardService {
                         pos: true,
                         atm: true,
                     },
-                    blockedCategories: ['zerocard'],
-                    allowedCategories: allowedCategories,
+                    blockedCategories: [],
+                    allowedCategories: [],
                     spendingLimits: [
                         {
                             interval: 'daily',
